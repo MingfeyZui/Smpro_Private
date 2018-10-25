58b8c1ab6fdd30caa3e456803b52c306b48e7752
Lalo <gerhardtl@cip.ifi.lmu.de>
2018-10-25 13:50:59 +0200

    Klasse aus dem Skript abgeschrieben

--- Laurin/oop_1_bank.py
+++ Laurin/oop_1_bank.py
@@ -24,14 +24,47 @@ e) Draw a UML diagram representing your Account class. (1 point)
 
 """
 
+#! /usr/bin/python3
+
+import re
 
 class Account:
-    """ Here has to be a documentation string that describes
-    which data objects this class is designed for.
-    You have to remove the pass statement and then write some
-    code for the class. """
-    pass
+    """Klassenattribute"""
+    num_of_accounts = 0
+
+    """ Methoden der Klasse Account. """
+    def withdraw(self, amount):
+        if amount > self.balance:
+            amount = self.balance
+        self.balance -= amount
+        return amount
+    def deposit(self, amount):
+        self.balance += amount
+    def __str__(self):
+        res = "*** Account Info ***\n"
+        res += "Account ID: " + str(self.number) + "\n"
+        res += "Holder: " + self.holder + "\n"
+        res += "Balance: " + str(self.balance) + "\n"
+        return res
+    def set_holder(self, person):
+        if (not type(person) == str):
+            raise TypeError
+        if (not re.match("\W+( \W+)*", person.strip())):
+            raise ValueError
+        self.holder = person
+    @staticmethod
+    def accounts_info():
+        print(Account.num_of_accounts, "accounts have been created")
+    
+    """ Konstruktor """
+    def __init__(self, num, person):
+        self.balance = 0
+        self.number = num
+        self.holder = person
+        Account.num_of_accounts += 1
+    
 
 if __name__ == "__main__":
     print("Welcome to the Python Bank!")
+
 

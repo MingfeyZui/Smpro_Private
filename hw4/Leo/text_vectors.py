2e90f4dfd7084f5b521b40fdb9171d652bb8168b
Leonhard Wabro <Leo.Wabro@campus.lmu.de>
2018-11-11 23:40:42 +0100

    doctest fertig

--- hw4/Leo/text_vectors.py
+++ hw4/Leo/text_vectors.py
@@ -4,27 +4,50 @@ import os, math
 
 
 def dot(dictA, dictB):
+    '''
+    >>> dot({"001":3, "002":8, "003":2, "004":14}, {"001":8, "002":2, "003":12})
+    64
+    >>> dot({"first": 0.5, "second": 2.5, "third": 0.05}, {"first": 3, "second": 5})
+    14.0
+    '''
     return sum([dictA.get(tok) * dictB.get(tok, 0) for tok in dictA])
 
 
 def normalized_tokens(text):
+    '''
+    >>> normalized_tokens("Hallo hier ist Leo")
+    ['hallo', 'hier', 'ist', 'leo']
+    >>> normalized_tokens("In Japan stellen Manga einen bedeutenden Teil der Literatur sowie der Medienlandschaft dar.")
+    ['in', 'japan', 'stellen', 'manga', 'einen', 'bedeutenden', 'teil', 'der', 'literatur', 'sowie', 'der', 'medienlandschaft', 'dar', '.']
+    '''
     return [token.lower() for token in word_tokenize(text)]
 
 
 class TextDocument:
     def __init__(self, text, id=None):
+        '''
+        Erschafft ein Objekt der Klasse TextDocument mit den Attributen
+        text und id, wobei id den default-wert None hat
+        '''
         self.text = text
         self.token_counts = FreqDist(normalized_tokens(text))
         self.id = id
 
     @classmethod
     def from_file(cls, filename):
+        '''
+        Erschafft ein Objekt der Klasse TextDocument aus einer Datei
+        '''
         with open(filename, 'r') as myfile:
             text = myfile.read().strip()
         return cls(text, filename)
 
 
 class DocumentCollection:
+        '''
+        Erschafft ein Objekt der Klasse DocumentCollection mit den 3 Attributen
+        term_to_df, term_to_docids, docid_to_doc
+        '''
     def __init__(self, term_to_df, term_to_docids, docid_to_doc):
         # string to int
         self.term_to_df = term_to_df
@@ -35,12 +58,19 @@ class DocumentCollection:
 
     @classmethod
     def from_dir(cls, dir, file_suffix):
+        '''
+        Übergibt der Methode cls.from_document_list eine Liste aller Dateien
+        mit der gleichen Endung im angegeben Verzeichnis
+        '''
         files = [(dir + "/" + f) for f in os.listdir(dir) if f.endswith(file_suffix)]
         docs = [TextDocument.from_file(f) for f in files]
         return cls.from_document_list(docs)
 
     @classmethod
     def from_document_list(cls, docs):
+        '''
+        Erschafft Objekte der Klasse DocumentCollection
+        '''
         term_to_df = defaultdict(int)
         term_to_docids = defaultdict(set)
         docid_to_doc = dict()

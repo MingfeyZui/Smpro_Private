c9eb2aed1f755cc238958a16f2ee8b42b39dc6c0
mingfei <cuimi@cip.ifi.lmu.de>
2018-12-20 16:32:56 +0100

    1

--- hw8/mingfei/analyze.py
+++ hw8/mingfei/analyze.py
@@ -21,6 +21,7 @@ class Analyzer(object):
     
     def lexicalDiversity(self):
         '''returns a list of lexical diversity of the text '''
+        # Höhe Diversity: mehr unterschiedliche Wörter
         return self.numberOfTokens()/self.vocabularySize()
 
     def getKeywords(self):
@@ -31,13 +32,19 @@ class Analyzer(object):
                 keys.append(key)
         return sorted(keys)
 
+        #Musterlösung     : iterier Typs , and iterier das Filter
+        #return sorted([w for w in self.token_counts.keys() if len(w)>7 and self.token_counts[w]>7])
+
     
     def numberOfHapaxes(self):
         '''returns the number of hapaxes in the text'''
         return len(self.token_counts.hapaxes())
     
     def avWordLength(self):
-        '''returns the average word length of the text'''
+        #此处题目表述为 所有不同词汇的平均值，而不是所有词汇的平均值'''returns the average word length of the text'''
+        #Musterlösung
+        #return sum([len(word) for word in self.token_counts])/len(self.token_counts)
+
         sumWordLen = 0
         for word in self.token_counts:
             sumWordLen = sumWordLen +len(word)
@@ -46,11 +53,21 @@ class Analyzer(object):
     def topSuffixes(self):
         '''returns the 10 most frequent 2-letter suffixes in words
             (restrict to words of length 5 or more)'''
+
+        #Musterlösung
+        #list_of_words = [word for word in self.token_counts if len(word) >=5]
+        #suf_dict = FreqDist(suf[-2:] for suf in list_of_words)
+        #suf_most_freq =[elem[0] for elem in suf_dict.most_common(10)]
+        #return suf_most_freq
+
+
         suffixes= []
         for langWord in self.token_counts.keys():
             if len(langWord) >= 5:
                 suffixes.append(langWord[-2:])
         return [word for word,count in Counter(suffixes).most_common(10)]
+
+
 
     
     def topPrefixes(self):
@@ -61,14 +78,17 @@ class Analyzer(object):
             if len(langWord) >= 5:
                 suffixes.append(langWord[:2])
         return [word for word,count in Counter(suffixes).most_common(10)]
-        #return sorted([word for word,count in Counter(suffixes).most_common(10)])
 
 
     def tokensTypical(self):
         """TODO returns first 5 tokens of the (alphabetically sorted) vocabulary 
         that contain both often sccleen prefixes and suffixes in the text. As in topPrefixes()
         and topSuffixes(), Prefixes and Suffixes are 2 characters long."""
-        pass
+
+        sufixes  = self.topSuffixes()
+        prefixes = self.topPrefixes()
+        return sorted([word for word in self.token_counts.keys() if word[:2] in prefixes and word[-2:] in sufixes])[:5]
+
                 
         
             
